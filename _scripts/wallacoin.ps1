d985d4b75dca829fe0675996406ae2ade99b6693
Richard <daw@richardyuste.com>
2025-03-03 16:31:20 +0100

    Updated scripts

--- _scripts/wallacoin.ps1
+++ _scripts/wallacoin.ps1
@@ -1,15 +1,17 @@
 # Open Wallacoin in VSC:
 #Start-Process "C:\Users\Richard\AppData\Local\Programs\Microsoft VS Code\Code.exe" "F:\DAW\projects\wallacoin-node"
-code F:\DAW\projects\wallacoin-node\
+code F:\DAW\projects\wallacoin-rest
+code F:\DAW\projects\wallacoin-angular
 
 #Chrome tabs:
 $url1 = "https://chatgpt.com/"
-$url2 = "http://localhost:8080/"
-$url3 = "https://github.com/RichardYusteDAW/notes"
-$file = "F:\DAW\projects\wallacoin\design\2.mockUp\MockUp2.pdf"
+$url2 = "http://localhost:4200"
+$url3 = "http://localhost:8081"
+$url4 = "https://github.com/RichardYusteDAW/notes/blob/master/JavaScript/frameworks/Angular/angular.md#112-de-hijo-al-padre-output"
+$url5 = "https://github.com/RichardYusteDAW/notes"
 
 # Open Chrome with URLs:
-Start-Process "chrome.exe" "$url1 $url2 $url3 $file"
+Start-Process "chrome.exe" "$url1 $url2 $url3 $url4 $url5"
 
 # Open Wallacoin directory:
 Invoke-Item "F:\DAW\projects\wallacoin-node"
