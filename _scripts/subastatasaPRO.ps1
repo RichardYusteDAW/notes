3f3d6f405da28532da7f5927db5f346ac0aa1f18
Richard <daw@richardyuste.com>
2026-04-14 17:22:36 +0200

    Created subastatasaPRO.ps1

---
+++ _scripts/subastatasaPRO.ps1
@@ -0,0 +1,11 @@
+# Open SubastasPRO in VSC:
+code D:\Escritorio\SubastasPRO\back
+code D:\Escritorio\SubastasPRO\front
+
+#Chrome tabs:
+$url1 = "https://chatgpt.com/"
+$url2 = "https://github.com/RichardYusteDAW/notes"
+$url3 = "http://localhost:4200"
+
+# Open Chrome with URLs:
+Start-Process "chrome.exe" "$url1 $url2 $url3"
